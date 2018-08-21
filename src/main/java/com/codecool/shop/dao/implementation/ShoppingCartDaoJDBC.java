cab4cfbeacc8f166f71902acd6dc8011f50d73c1
timardaniel <daniel.timar21@gmail.com>
2018-08-21 18:21:38 +0200

    queries for the shopping Cart and make a shopping cart class, but we have to change the interface, good luck tomorrow

--- src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoJDBC.java
@@ -3,35 +3,63 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.jdbc.JDBCController;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.ShoppingCart;
+import com.codecool.shop.model.Supplier;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 
-public class ShoppingCartDaoJDBC implements ShoppingCartDao {
+public class /*ShoppingCartDaoJDBC implements*/ ShoppingCartDaoJDBC {
     private static final JDBCController controller = JDBCController.getInstance();
 
-    @Override
-    public void add(Product product) {
+    public List<ShoppingCart> executeQueryWithReturnValue(String query) {
+        List<ShoppingCart> resultList = new ArrayList<>();
 
+        try (Connection connection = controller.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                ShoppingCart data = new ShoppingCart(resultSet.getInt("user_id"),
+                        resultSet.getDate("time"),
+                        resultSet.getString("status"));
+
+                resultList.add(data);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
     }
 
-    @Override
-    public Product find(int id) {
-        return null;
+    //product-ba add? másképp? plusz modell S Cart
+    public void addToDatabase(ShoppingCart shoppingCart) {
+        controller.executeQuery("INSERT INTO shopping_cart (id, user_id, time, status) VALUES (DEFAULT, " +
+                shoppingCart.getUserId() + ", '" + shoppingCart.getTime() + "', '" + shoppingCart.getStatus()+ "';");
     }
 
-    @Override
-    public Product find(String name) {
-        return null;
+    public ShoppingCart findUserCart(int id){
+        return executeQueryWithReturnValue("SELECT * FROM shopping_cart WHERE id = '" + id + "';").get(0);
     }
 
-    @Override
-    public void remove(int id) {
-
+    public ShoppingCart findFUCK(String name) {
+        return executeQueryWithReturnValue("SELECT * FROM shopping_cart WHERE id = '" + name + "';").get(0);
     }
 
-    @Override
-    public Set<Product> getAll() {
-        return null;
+
+    public void removeShoppingCart(int id) { controller.executeQuery("DELETE FROM shopping_cart WHERE id = '" + id + "';"); }
+
+    public List<ShoppingCart> getAllCart() {
+        return executeQueryWithReturnValue("SELECT * FROM shopping_cart");
     }
+
 }
