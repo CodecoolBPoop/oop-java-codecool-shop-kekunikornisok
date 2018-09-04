b42c69e25a227e6f4a96e5aa001e65fe0f01e049
Grob Oliver <groboliver1117@gmail.com>
2018-09-04 13:32:34 +0200

    All JDBC classes updated to use preparedStatement

--- src/main/java/com/codecool/shop/dao/ShippingAddressDao.java
+++ src/main/java/com/codecool/shop/dao/ShippingAddressDao.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public interface ShippingAddressDao {
 
-    void add(int id, String country, String city, String address, String zipCode);
+    void add(int userId, String country, String city, String address, String zipCode);
     List<Integer> getUserId();
-    void setTable(int id, String country, String city, String address, String zipCode);
+    void setTable(int userId, String country, String city, String address, String zipCode);
 
 
 }
--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -6,11 +6,10 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ProductDaoJDBC implements ProductDao {
@@ -24,14 +23,20 @@ public class ProductDaoJDBC implements ProductDao {
         return instance;
     }
 
-    public List<Product> executeQueryWithReturnValue(String query) {
+    private List<Product> executeQueryWithReturnValue(String query, List<Object> parameters) {
+        Connection connection = controller.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
         List<Product> resultList = new ArrayList<>();
 
-        try (Connection connection = controller.getConnection();
-             Statement statement =connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query);
-        ){
-            while (resultSet.next()){
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            for (int i = 0; i < parameters.size(); i++) {
+                preparedStatement.setObject(i + 1, parameters.get(i));
+            }
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
                 Product data = new Product(resultSet.getInt("id"),
                         resultSet.getString("name"),
                         resultSet.getFloat("default_price"),
@@ -44,46 +49,69 @@ public class ProductDaoJDBC implements ProductDao {
 
         } catch (SQLException e) {
             e.printStackTrace();
+
+        } finally {
+            try { if (resultSet != null) resultSet.close(); } catch (SQLException e) { e.printStackTrace(); }
+            try { if (preparedStatement != null) preparedStatement.close(); } catch (SQLException e) { e.printStackTrace(); }
+            try { if (connection != null) connection.close(); } catch (SQLException e) { e.printStackTrace(); }
         }
 
         return resultList;
     }
 
     @Override
-    public void add(Product product) {
-        int supplierId = product.getSupplier().getId();
-        int productCategoryId = product.getProductCategory().getId();
-        controller.executeQuery("INSERT INTO product (id, name, description, default_price, currency_string, supplier_id, product_category_id) VALUES (DEFAULT, '" +
-                product.getName() + "', '" + product.getDescription() + "', " + product.getDefaultPrice() + ", " + product.getDefaultCurrency() + ", " + supplierId + ", " + productCategoryId + ";");
+    public void add(String name, float defaultPrice, String currencyString, String description, ProductCategory productCategory, Supplier supplier) {
+        controller.executeQuery(
+        "INSERT INTO product (id, name, description, default_price, currency_string, supplier_id, product_category_id) " +
+                "VALUES (DEFAULT, ?, ?, ?, ?, ?, ?);",
+            Arrays.asList(name, description, defaultPrice, currencyString, supplier.getId(), productCategory.getId())
+        );
     }
 
     @Override
     public Product find(int id) {
-        return executeQueryWithReturnValue("SELECT * FROM product WHERE id = '" + id + "';").get(0);
+        List<Product> products = executeQueryWithReturnValue(
+        "SELECT * FROM product WHERE id = ?;",
+            Collections.singletonList(id));
+
+        return (products.size() != 0) ? products.get(0) : null;
     }
 
     @Override
     public Product find(String name) {
-        return executeQueryWithReturnValue("SELECT * FROM product WHERE name LIKE '" + name + "';").get(0);
+        List<Product> products = executeQueryWithReturnValue(
+        "SELECT * FROM product WHERE name LIKE ?;",
+            Collections.singletonList(name));
+
+        return (products.size() != 0) ? products.get(0) : null;
     }
 
     @Override
     public void remove(int id) {
-        controller.executeQuery("DELETE FROM product WHERE id = '" + id + "';");
+        controller.executeQuery(
+        "DELETE FROM product WHERE id = ?;",
+            Collections.singletonList(id));
     }
 
     @Override
     public List<Product> getAll() {
-        return executeQueryWithReturnValue("SELECT * FROM product");
+        return executeQueryWithReturnValue(
+        "SELECT * FROM product;",
+            Collections.emptyList());
     }
 
     @Override
-    public List<Product> getBy(Supplier supplier) {
-        return executeQueryWithReturnValue("SELECT * FROM product WHERE supplier_id = '" + supplier.getId() + "';");
+    public List<Product> getBySupplier(int supplierId) {
+        return executeQueryWithReturnValue(
+        "SELECT * FROM product WHERE supplier_id = ?;",
+            Collections.singletonList(supplierId));
     }
 
     @Override
-    public List<Product> getBy(ProductCategory productCategory) {
-        return executeQueryWithReturnValue("SELECT * FROM product WHERE product_category_id = '" + productCategory.getId() + "';");
+    public List<Product> getByProductCategory(int productCategoryId) {
+        return executeQueryWithReturnValue(
+        "SELECT * FROM product WHERE product_category_id = ?;",
+            Collections.singletonList(productCategoryId));
     }
+
 }
