919f40ec2dc5a43a6def665eaeb5bc452d6eca2d
Grob Oliver <groboliver1117@gmail.com>
2018-09-06 10:38:41 +0200

    On wrong user login input we correctly get an error message

--- src/main/java/com/codecool/shop/dao/implementation/UserDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoJDBC.java
@@ -144,6 +144,6 @@ public class UserDaoJDBC implements UserDao {
             return false;
         }
 
-        return user.getEmailAddress().equals(email) && user.getPassword().equals(password);
+        return (user != null) && (user.getEmailAddress().equals(email) && user.getPassword().equals(password));
     }
 }
